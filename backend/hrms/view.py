0744f2d00c55d6b2792d7744a3f58842025d638e
cwaq567@163.com <cwaq567@163.com>
2020-12-20 15:01:27 +0800

    个人信息查询数据库联调成功

--- backend/hrms/view.py
+++ backend/hrms/view.py
@@ -1,10 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 import json
-import pymysql
-import re
-import time
-import codecs
+from .sql_operator import *
+
 
 # 获取request的body部分携带的信息
 def getBody(request):
@@ -29,19 +27,8 @@ def login(request):
 # 获取个人信息
 def getUserInfo(request):
     body = getBody(request)
-    res = json.dumps({
-        'status': True,
-        'data':{
-            'name':'youky',
-            'sex':'男',
-            'phone':'15623687738',
-            'email':'youkyf@qq.com',
-            'department':'jsj1803',
-            'position':'student',
-            'hireDate':'2018.9',
-            'experience':[{'description':'小学','grade':90},{'description':'中学','grade':90},{'description':'高中','grade':90},{'description':'高中','grade':90},{'description':'高中','grade':90},{'description':'高中','grade':90},{'description':'高中','grade':90}]
-        }
-    })
+    res = json.dumps(getUserinfo(db,kwargs = body))
+    print('res is ',res)
     return HttpResponse(res)
 
 # 获取公司的岗位信息
